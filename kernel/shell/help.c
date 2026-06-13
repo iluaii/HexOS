8fdf9d6ecb10bd0f5ae715aa436c0a689b419c75
iluaii <nikto0576@gmail.com>
2026-06-13 19:07:21 +0300

    updated help.c

--- kernel/shell/help.c
+++ kernel/shell/help.c
@@ -1,7 +1,13 @@
 #include "../../include/vga.h"
 
 void help(int argc, char **argv) {
-    print("cat usage: cat <file>\n");
-    print("ls print dirs\n");
-    print("clear\n");
+    print("ls - list files in the virtual filesystem\n");
+    print("cat <file> - print file contents\n");
+    print("clear - clear the screen\n");
+    print("poweroff - poweroff of system\n");
+    print("reboot - reboot of system\n");
+    print("mem - output of used RAM\n");
+    print("uname - output of system version\n");
+    print("echo - print text\n");
+    print("neofetch - print logo\n");
 }
